a9c903d9fda98b4c34792d21e07d16107c7b8e80
Onur Baykal <theaob@gmail.com>
2013-01-05 13:26:03 +0200

    added sound

--- CrystallBall/gen/com/onurbaykal/crystallball/R.java
+++ CrystallBall/gen/com/onurbaykal/crystallball/R.java
@@ -11,7 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int twenty=0x7f040000;
+        public static final int twenty=0x7f050000;
     }
     public static final class drawable {
         public static final int ball01=0x7f020000;
@@ -42,25 +42,28 @@ public final class R {
         public static final int ic_launcher=0x7f020019;
     }
     public static final class id {
-        public static final int button1=0x7f080001;
-        public static final int imageView1=0x7f080000;
-        public static final int menu_settings=0x7f080005;
-        public static final int textView1=0x7f080003;
-        public static final int view1=0x7f080002;
-        public static final int view2=0x7f080004;
+        public static final int button1=0x7f090001;
+        public static final int imageView1=0x7f090000;
+        public static final int menu_settings=0x7f090005;
+        public static final int textView1=0x7f090003;
+        public static final int view1=0x7f090002;
+        public static final int view2=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
+        public static final int activity_main=0x7f080000;
+    }
+    public static final class raw {
+        public static final int crystal_ball=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int backgroundDescription=0x7f050004;
-        public static final int englightenButtonText=0x7f050003;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
+        public static final int app_name=0x7f060000;
+        public static final int backgroundDescription=0x7f060004;
+        public static final int englightenButtonText=0x7f060003;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
     }
     public static final class style {
         /** 
@@ -84,10 +87,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
