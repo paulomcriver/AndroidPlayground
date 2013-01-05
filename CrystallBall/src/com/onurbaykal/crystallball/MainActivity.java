78c8abf46b604fd0f636a8b6507b13fe002cbb8b
Onur Baykal <theaob@gmail.com>
2013-01-05 13:15:02 +0200

    added animations

--- CrystallBall/src/com/onurbaykal/crystallball/MainActivity.java
+++ CrystallBall/src/com/onurbaykal/crystallball/MainActivity.java
@@ -1,28 +1,55 @@
 package com.onurbaykal.crystallball;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	
+	private ImageView mBackImageView;
+	private TextView mAnswerLabel;
+	private Button mAnswerButton;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
-        Button getAnswerButton = (Button) findViewById(R.id.button1); 
-        getAnswerButton.setOnClickListener(new View.OnClickListener() {
+        mBackImageView = (ImageView) findViewById(R.id.imageView1);
+        mAnswerLabel = (TextView) findViewById(R.id.textView1);
+        mAnswerButton = (Button) findViewById(R.id.button1); 
+        mAnswerButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				answerLabel.setText(CrystallBall.getAnAnswer());
+				mAnswerLabel.setText(CrystallBall.getAnAnswer());
+				animateCrystallBall();
 			}
 		});
+    }
+    
+    private void animateCrystallBall() {
+    	mBackImageView.setImageResource(R.drawable.ball_animation);
+    	AnimationDrawable ballAnimation = (AnimationDrawable) mBackImageView.getDrawable();
+    	if(ballAnimation.isRunning())
+    	{
+    		ballAnimation.stop();
+    	}
+    	ballAnimation.start();
+    	animateAnswer();
+    }
+    
+    private void animateAnswer() {
+    	AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1);
+    	fadeInAnimation.setDuration(1500);
+    	fadeInAnimation.setFillAfter(true);
+    	mAnswerLabel.setAnimation(fadeInAnimation);
     }
 
     @Override
